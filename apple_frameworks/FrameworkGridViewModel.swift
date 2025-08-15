0a8e645b5b5edfd57a525e208f4ff9ecfc4a8be9
siyatut <anastasiya.tyutinova@gmail.com>
2025-08-15 15:24:37 +0300

    Set up FrameworkGridViewModel

---
+++ apple_frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  FrameworkGridViewModel.swift
+//  apple_frameworks
+//
+//  Created by Anastasia Tyutinova on 15/8/2568 BE.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
+}
