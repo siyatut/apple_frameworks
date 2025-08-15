4f7f2fe4759de20e5f3a8aedc32803065d54672e
siyatut <anastasiya.tyutinova@gmail.com>
2025-08-15 12:30:02 +0300

    First attempt with grid

--- apple_frameworks/FrameworkGridView.swift
+++ apple_frameworks/FrameworkGridView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+        ContainerRelativeShape()
+                .fill(Color.black.gradient)
+                .ignoresSafeArea()
+            LazyVGrid(columns: columns) {
+                FrameworkTitleView(name: "App Clips", imageName: "app-clips")
+                FrameworkTitleView(name: "App Clips", imageName: "app-clips")
+                FrameworkTitleView(name: "App Clips", imageName: "app-clips")
+            }
+        }
     }
 }
 
 #Preview {
     FrameworkGridView()
+}
+
+struct FrameworkTitleView: View {
+    
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+                .foregroundStyle(.white)
+            
+        }
+    }
 }
